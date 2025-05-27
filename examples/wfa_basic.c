6ad4c59ef8ba6cb3c38f28cbfb4a5f4ce02c8b9e
TheBanana03 <dane_chan@dlsu.edu.ph>
2025-05-27 08:17:58 +0000

    extend optimizarions

--- examples/wfa_basic.c
+++ examples/wfa_basic.c
@@ -77,13 +77,13 @@ int main(int argc, char* argv[]) {
     
   // fprintf(stderr,"WFA-Alignment returns score %d\n",wf_aligner->cigar->score);
 
-  // // Display alignment
-  fprintf(stderr,"  PATTERN  %s\n",pattern);
-  fprintf(stderr,"  TEXT     %s\n",text);
-  fprintf(stderr,"  SCORE (RE)COMPUTED %d\n",
-       cigar_score_gap_affine(wf_aligner->cigar,&attributes.affine_penalties));
-  cigar_print_pretty(stderr,wf_aligner->cigar,
-      pattern,strlen(pattern),text,strlen(text));
+  // Display alignment
+  // fprintf(stderr,"  PATTERN  %s\n",pattern);
+  // fprintf(stderr,"  TEXT     %s\n",text);
+  // fprintf(stderr,"  SCORE (RE)COMPUTED %d\n",
+  //      cigar_score_gap_affine(wf_aligner->cigar,&attributes.affine_penalties));
+  // cigar_print_pretty(stderr,wf_aligner->cigar,
+  //     pattern,strlen(pattern),text,strlen(text));
 
      long long elapsed_time = (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
     
